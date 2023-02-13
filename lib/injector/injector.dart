7c37de940f03944ff15c989120b6f5619e5e107f
fazle122 <fazlerabby122@gmail.com>
2023-02-13 19:22:37 +0600

    Initial commit

---
+++ lib/injector/injector.dart
@@ -0,0 +1,34 @@
+import 'package:boilerplate/injector/database_module.dart';
+import 'package:boilerplate/injector/repository_module.dart';
+import 'package:flutter/foundation.dart';
+import 'package:get_it/get_it.dart';
+
+import 'bloc_module.dart';
+import 'rest_client_module.dart';
+import 'service_module.dart';
+
+class Injector {
+  static GetIt instance = GetIt.instance;
+  Injector._();
+
+  static void init() {
+    ServiceModule.init();
+    RestClientModule.init();
+
+    if (!kIsWeb) {
+      DatabaseModule.init();
+      DatabaseModule.initRepositories();
+    }
+
+    RepositoryModule.init();
+    BlocModule.init();
+  }
+
+  static void reset() {
+    instance.reset();
+  }
+
+  static void resetLazySingleton() {
+    instance.resetLazySingleton();
+  }
+}
